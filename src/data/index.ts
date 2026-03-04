5e71ec8409e67c5428473c1cc580a5b6749b89ab
Henry Henriques <henrique.henriques@cognite.com>
2026-03-04 22:02:01 +0100

    Added creamy beef mushroom pasta bake

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,17 +1,9 @@
 import type { Recipe } from "../types";
 
-import spaghettiCarbonara from "./spaghetti-carbonara.json";
-import chickenTikkaMasala from "./chicken-tikka-masala.json";
-import greekSalad from "./greek-salad.json";
-import bananaPancakes from "./banana-pancakes.json";
-import beefTacos from "./beef-tacos.json";
+import creamyBeefMushroomPastaBake from "./creamy-beef-mushroom-pasta-bake.json";
 
 const recipes: Recipe[] = [
-  spaghettiCarbonara,
-  chickenTikkaMasala,
-  greekSalad,
-  bananaPancakes,
-  beefTacos,
+  creamyBeefMushroomPastaBake,
 ];
 
 export default recipes;
