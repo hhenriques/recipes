ae3a8e8fe8237b4991f07a6daba31f6c226ac990
Henry Henriques <henrique.henriques@cognite.com>
2026-03-04 22:20:02 +0100

    Added more recipes

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,9 +1,17 @@
 import type { Recipe } from "../types";
 
 import creamyBeefMushroomPastaBake from "./creamy-beef-mushroom-pasta-bake.json";
+import soyGlazedChickenPepper from "./soy-glazed-chicken-pepper.json";
+import chickenMacaroniLemonPiccata from "./chicken-macaroni-lemon-piccata.json";
+import chickenGarlicParmesan from "./chicken-garlic-parmesan.json";
+import stickyLemonChickenCrunchyPotatoes from "./sticky-lemon-chicken-crunchy-potatoes.json";
 
 const recipes: Recipe[] = [
   creamyBeefMushroomPastaBake,
+  soyGlazedChickenPepper,
+  chickenMacaroniLemonPiccata,
+  chickenGarlicParmesan,
+  stickyLemonChickenCrunchyPotatoes,
 ];
 
 export default recipes;
